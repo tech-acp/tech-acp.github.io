61035d3d28fba20bfe2f1ef8db8c1ef73ec037c3
Hugo Faye <hugo.faye@gmail.com>
2023-01-11 17:27:26 +0100

    put site to root folder

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package fr.acp.suivi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/custom_code/widgets/index.dart
+++
@@ -1,2 +0,0 @@
-export 'open_street_map.dart' show OpenStreetMap;
-export 'custom_map.dart' show CustomMap;
